049a3e7ae24dcfb04a5be8f8378ee1c0c007e432
Gadiel Us <gadyus18@gmail.com>
2022-08-22 19:28:31 -0600

    Este es el proyecto que ya habiamos comenzado

---
+++ Letras_edad/Letras_edad/Letras_edad.cpp
@@ -0,0 +1,27 @@
+#include<conio.h>
+#include<iostream>
+#include<map>
+using namespace std;
+
+
+
+int main()
+{
+	int edad;
+	string telefono;
+	string nomb;
+
+	cout << "Ingrese su edad " << endl;
+	cin >> edad;
+
+	cout << "Ingrese su numero telefonico " << endl;
+	cin >> telefono;
+
+	cout << "Ingrese su nombre " << endl;
+	cin >> nomb;
+
+	cout << endl << nomb << endl << edad << endl << telefono;
+
+
+
+};
